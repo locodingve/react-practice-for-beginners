ae2f61154425db9ba400d80b76d3f8e9b4359e9c
locodingve <locodingve@gmail.com>
2023-04-26 16:39:47 +0900

    feat: add  useEffect 2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,32 @@ import styles from './App.module.scss';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
+  const onChange = (e) => setKeyword(e.target.value);
+
+  console.log('I run all the time');
   useEffect( () => {
-    console.log("CALL THE API .....");
+    console.log('CALL THE API .....');
   }, []);
+  useEffect(() => {
+    console.log('I run when "keyword" changes.');
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when "counter" changes.');
+  }, [counter]);
+  useEffect(() => {
+    console.log('I run when keyword & counter change');
+  }, [keyword, counter]);
   return (
     <div>
+      <input 
+        value={keyword} 
+        onChange={onChange}
+        type='text'
+        placeholder='Search here ...'
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button onClick={onClick} text={'click me'} />
     </div>
