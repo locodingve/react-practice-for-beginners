f042bd1d8b5941044061295fc5c276ca8b0e4cc7
locodingve <locodingve@gmail.com>
2023-04-26 16:25:39 +0900

    feat: add useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from './Botton';
 import styles from './App.module.scss';
 
@@ -6,7 +6,10 @@ import styles from './App.module.scss';
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("I run all the time");
+  useEffect( () => {
+    console.log("CALL THE API .....");
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
